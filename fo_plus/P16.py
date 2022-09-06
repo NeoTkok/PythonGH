92204dd7b7e465cb89ab282941cd544c9d381928
NeoDima <murdereruu@yandex.ru>
2022-09-07 01:49:27 +0300

    Added task 1.6

---
+++ fo_plus/P16.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+
+
+
+x,y = input().split(" ")
+
+z = []
+for i in range(0,int(x)+100):
+    z.append([0]* (int(y)+100))
+
+
+N = int(input())
+print(z[1][3])
+
+for i in range(0, N):    
+    xmin, xmax, ymin, ymax = input().split(" ")
+
+    for j in range(int(ymin), int(ymax)):
+        for k in range(int(xmin), int(xmax)):
+            z[j][k] = z[j][k] + 1
+
+print(z)

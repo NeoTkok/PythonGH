cb842a0d61744df5c01fee99399e8f4900ff013a
NeoDima <murdereruu@yandex.ru>
2022-09-11 10:19:58 +0300

    ++

--- fo_plus/P16.py
+++ fo_plus/P16.py
@@ -1,20 +1,28 @@
-import numpy as np
+INP = input().split(" ") # размеры стены
+x, y = int(INP[0]), int(INP[1])
 
-x,y = input().split(" ")
-
+N = int(input()) # колличесво окон
 z = []
-for i in range(0,int(x)+100):
-    z.append([0]* (int(y)+100))
+m = []
+m.append([1]*x)
+for i in range(1,y-1):
+    m.append([1]+[0]*(x-2)+[1])
+m.append([1]*x)
+
+#"""
+for i in range(0, N): 
+    z.append([int(s) for s in input().split(" ")])
+
+for i in range(0, N):
+
+    for j in range(z[i][0],z[i][2]):
+        for k in range(z[i][1],z[i][3]):
+            m[k][j] = 1
 
 
-N = int(input())
-print(z[1][3])
-
-for i in range(0, N):    
-    xmin, xmax, ymin, ymax = input().split(" ")
-
-    for j in range(int(ymin), int(ymax)):
-        for k in range(int(xmin), int(xmax)):
-            z[j][k] = z[j][k] + 1
-
-print(z)
+#"""
+sumM = 0
+for i in range(0,y):
+    sumM = sum(m[i]) + sumM 
+    print(m[i])
+print(sumM)
